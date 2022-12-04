049a8e63958d9eb4f58b56ec8eae3e0865782631
ankur singh <68726977+ankur2802@users.noreply.github.com>
2022-12-04 22:57:49 +0530

    Update Question_2.java

--- Question_2.java
+++ Question_2.java
@@ -1,4 +1,4 @@
-/*import java.util.*;
+import java.util.*;
 public class Question_2 {
 
 public static void main(String args[]){
@@ -11,33 +11,4 @@ public static void main(String args[]){
     }
 }
 
-}*/
-
-public class Question_2 {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
-        int n=10;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-
-                if((i==0 && j<n-1)|| j==0  ||(i==n-1 && j<n-1) || (j==n-1 &&i>0 &&i<n-1) )
-                {
-                    System.out.print("*");
-                }
-                else
-                {
-                    System.out.print(" ");
-                }
-
-            }
-            System.out.println();
-        }
-
-    }
-
 }
-
